154a56a913f19e8920362eb001a6893bc6f48fdb
Umar Saidu Auna <umarmanofpeace@gmail.com>
2021-01-13 22:38:35 +0100

    fix: 🐛 Change the UI of ceasarCrypt

--- app/src/main/java/buk/project/mini/caesarsCrypt/Decode.kt
+++ app/src/main/java/buk/project/mini/caesarsCrypt/Decode.kt
@@ -1,16 +1,26 @@
 package buk.project.mini.caesarsCrypt
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Decode : AppCompatActivity() {
     lateinit var keyNumber: NumberPicker
     lateinit var editDecrypt: EditText
     lateinit var btnDecrypt: Button
     lateinit var btnClearDecrypt: Button
+    private lateinit var clipboardManager: ClipboardManager
+    private lateinit var clipData: ClipData
+    private lateinit var btnPaste: Button
+    private lateinit var btnShare: FloatingActionButton
+    private lateinit var btnCopy: FloatingActionButton
     lateinit var txtDecrypt: TextView
     private lateinit var space: String
     lateinit var decrypt: String
@@ -26,11 +36,41 @@ class Decode : AppCompatActivity() {
         btnClearDecrypt = findViewById(R.id.btnDecryptClear)
         txtDecrypt = findViewById(R.id.textViewDecrypt)
         keyNumber = findViewById(R.id.numberPickerDecrypt)
+        btnShare = findViewById(R.id.shareOthersDecrypt)
+        btnCopy = findViewById(R.id.copyClipboardDecrypt)
+        btnPaste = findViewById(R.id.btnPasteDecrypt)
         keyNumber.maxValue = 256
         keyNumber.minValue = 1
         keyNumber.value = 1
         decrypt()
         clearDecode()
+        clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+        btnPaste.setOnClickListener {
+            val paste = clipboardManager.primaryClip
+            val item = paste?.getItemAt(0)
+            editDecrypt.setText(item?.text.toString())
+        }
+
+        btnCopy.setOnClickListener {
+            val textCopy = txtDecrypt.text.toString()
+
+            clipData = ClipData.newPlainText("text", textCopy)
+            clipboardManager.setPrimaryClip(clipData)
+            Toast.makeText(this, "Text copied to clipboard", Toast.LENGTH_LONG).show()
+
+        }
+
+        btnShare.setOnClickListener {
+            val shareText = txtDecrypt.text.toString()
+
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText)
+            startActivity(Intent.createChooser(shareIntent, "Share via"))
+
+        }
     }
 
     private fun decrypt() {
--- app/src/main/java/buk/project/mini/caesarsCrypt/Encode.kt
+++ app/src/main/java/buk/project/mini/caesarsCrypt/Encode.kt
@@ -1,17 +1,27 @@
 package buk.project.mini.caesarsCrypt
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Encode : AppCompatActivity() {
     lateinit var keyNumber: NumberPicker
     private lateinit var btnEncrypt: Button
     private lateinit var btnClearEncrypt: Button
+    private lateinit var clipboardManager: ClipboardManager
+    private lateinit var clipData:ClipData
+    lateinit var btnPaste: Button
     private lateinit var txtEncrypt: TextView
     lateinit var editEncrypt: EditText
+    lateinit var btnShare: FloatingActionButton
+    lateinit var btnCopy: FloatingActionButton
     private lateinit var space: String
     lateinit var encrypt: String
     private var shiftKey = 0
@@ -26,11 +36,41 @@ class Encode : AppCompatActivity() {
         btnClearEncrypt = findViewById(R.id.btnEncryptClear)
         txtEncrypt = findViewById(R.id.textViewEncrypt)
         keyNumber = findViewById(R.id.numberPickerencrypt)
+        btnShare = findViewById(R.id.shareOthers)
+        btnCopy = findViewById(R.id.copyClipboard)
+        btnPaste = findViewById(R.id.btnPaste)
         keyNumber.maxValue = 256
         keyNumber.minValue = 1
         keyNumber.value = 1
         encrypt()
         clearEncode()
+        clipboardManager =  getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+        btnPaste.setOnClickListener {
+            val paste = clipboardManager.primaryClip
+            val item = paste?.getItemAt(0)
+            editEncrypt.setText(item?.text.toString())
+        }
+
+        btnCopy.setOnClickListener {
+            val textCopy = txtEncrypt.text.toString()
+
+            clipData = ClipData.newPlainText("text", textCopy)
+            clipboardManager.setPrimaryClip(clipData)
+            Toast.makeText(this, "Text copied to clipboard", Toast.LENGTH_LONG).show()
+
+        }
+
+        btnShare.setOnClickListener {
+            val shareText = txtEncrypt.text.toString()
+
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText)
+            startActivity(Intent.createChooser(shareIntent, "Share via"))
+
+        }
     }
 
     private fun encrypt() {
